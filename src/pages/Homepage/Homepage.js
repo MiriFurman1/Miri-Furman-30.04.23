7c43653d074c2d3d2c99e734e0ed91771b2e0eec
Miri <mirifurman@gmail.com>
2023-05-01 13:22:45 +0300

    search is working with autocomplete

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -7,54 +7,95 @@ import useFavoritesStore from '../../app/favoritesStore.js';
 import './Homepage.css'
 import axios from 'axios';
 import dayjs from 'dayjs';
+import { AiOutlineSearch } from "react-icons/ai";
+
 function Homepage() {
     const addFavorite = useFavoritesStore((state) => state.addFavorite)
     const removeFavorite = useFavoritesStore((state) => state.removeFavorite)
     const [currentWeather, setCurrentWeather] = useState("")
     const [currentLocation, setCurrentLocation] = useState("Tel Aviv")
     const [daysForecasts, setDaysForecasts] = useState("")
+    const [citySearch, setCitySearch] = useState("")
+    const [cityData, setCityData] = useState("")
 
     useEffect(() => {
-        // axios.get(`http://dataservice.accuweather.com/currentconditions/v1/215854?apikey=${process.env.REACT_APP_API_KEY}`)
-        // .then((response)=>{
-        //     setCurrentWeather(response.data)
-        //     localStorage.setItem("currentWeather",JSON.stringify(response.data))
-        //     console.log(response.data);
-        // })
-        // axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/215854?apikey=${process.env.REACT_APP_API_KEY}&metric=true`)
-        //         .then((response)=>{
-        //             setDaysForecasts(response.data)
-        //         localStorage.setItem("daysForecasts",JSON.stringify(response.data))
-        //         console.log(response.data);
-        //     })
+        axios.get(`http://dataservice.accuweather.com/currentconditions/v1/215854?apikey=${process.env.REACT_APP_API_KEY}`)
+        .then((response)=>{
+            setCurrentWeather(response.data)
+            localStorage.setItem("currentWeather",JSON.stringify(response.data))
+            console.log(response.data);
+        })
+        axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/215854?apikey=${process.env.REACT_APP_API_KEY}&metric=true`)
+                .then((response)=>{
+                    setDaysForecasts(response.data)
+                localStorage.setItem("daysForecasts",JSON.stringify(response.data))
+                console.log(response.data);
+            })
     }, [])
 
 
     useEffect(() => {
         setCurrentWeather(JSON.parse(localStorage.getItem("currentWeather")))
         setDaysForecasts(JSON.parse(localStorage.getItem("daysForecasts")))
-        console.log(currentLocation);
 
     }, [])
+
+    const searchCity = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.get(
+                `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${process.env.REACT_APP_API_KEY}&q=${citySearch}`
+            );
+            let cityDataVar=response.data[0];
+            setCityData(cityDataVar);
+            setCitySearch('');
+            setCurrentLocation(cityDataVar.LocalizedName)
+            console.log(cityDataVar);
+            const currentWeatherResponse = await axios.get(
+                `http://dataservice.accuweather.com/currentconditions/v1/${cityDataVar.Key}?apikey=${process.env.REACT_APP_API_KEY}`
+            );
+            const currentWeatherData = currentWeatherResponse.data;
+            setCurrentWeather(currentWeatherData);
+            localStorage.setItem('currentWeather', JSON.stringify(currentWeatherData));
+            const daysForecastsResponse = await axios.get(
+                `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityDataVar.Key}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
+            );
+            const daysForecastsData = daysForecastsResponse.data;
+            console.log(daysForecastsData)
+            setDaysForecasts(daysForecastsData);
+            localStorage.setItem('daysForecasts', JSON.stringify(daysForecastsData));
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+
     return (
-        <div className='Homepage'>
+        <div className='Homepage' >
             <Header />
-            <input></input>
-            <Container>
-                <div className='topContainer'>
-                    <Card style={{ width: '200px' }}>
+            <Container >
+                <form className='search' onSubmit={searchCity}>
+                    <input
+                        value={citySearch}
+                        onChange={(e) => setCitySearch(e.target.value)}>
+                    </input>
+                    <Button type='submit' style={{ backgroundColor: 'white', padding: '0.5rem' }}><AiOutlineSearch /></Button>
+                </form>
+
+                <div className='topContainer' >
+                    <Card style={{ width: '200px', padding: '0.5rem', textAlign: 'center' }}>
                         {currentLocation && <h3>{currentLocation}</h3>}
                         {currentWeather[0] && <h3>{currentWeather[0].Temperature.Metric.Value}{currentWeather[0].Temperature.Metric.Unit}</h3>}
                         {currentWeather[0] && <h3>{currentWeather[0].WeatherText}</h3>}
                         {currentWeather[0] && <img src={`/weather icons/${currentWeather[0].WeatherIcon}-s.png`} alt='day-icon'></img>}
                     </Card>
-                    <Button>Add to favorites</Button>
+                    <Button style={{ backgroundColor: 'white', padding: '0.5rem' }}>Add to favorites</Button>
                 </div>
-                {currentLocation && <h3>{currentLocation}</h3>}
                 <div className='DaysWeather'>
                     {daysForecasts && daysForecasts.DailyForecasts.map((forecast, i) => {
                         return (
-                            <Card key={i} style={{ width: '200px' }}>
+                            <Card key={i} style={{ width: '200px', padding: '0.5rem', textAlign: 'center' }}>
                                 <p>{dayjs(forecast.Date).format(' DD-MM-YYYY')}</p>
                                 <p>{forecast.Temperature.Maximum.Value}{forecast.Temperature.Maximum.Unit}</p>
                                 <img src={`/weather icons/${forecast.Day.Icon}-s.png`} alt='day-icon'></img>
